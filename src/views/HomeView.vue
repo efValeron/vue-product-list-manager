83df0d7b9563a8ecb999dea4484dafa303baf3d6
Valeriy <masterbox06@gmail.com>
2024-09-02 14:45:44 +0500

    init shadcn components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <main>
     <h1 class="text-4xl font-light">hello tailwind</h1>
+    <Input/>
+    <Button>Send</Button>
   </main>
 </template>
